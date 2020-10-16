7843615807a7e5acfc46c5d3d5ad2ba6bc3e5dea
LEFEBVRE Guillaume <gu.lefebvre@student.isartdigital.com>
2020-10-16 16:11:20 +0200

    Add comment

--- Include/Vector/baseVector.hpp
+++ Include/Vector/baseVector.hpp
@@ -5,10 +5,10 @@
 /**
  * @brief Base class for vector with N dimension. Musn't be instancied, but only inherited.
  *
- * @tparam CHILD Child class.
  * @tparam DIM Number of dimension.
  * @tparam TYPE Type of value stored. Default = float.
 */
+
 template <size_t DIM, typename TYPE = float>
 class BaseVector
 {
@@ -22,12 +22,38 @@ protected:
 public:
 
 #pragma region Static Methods
+    /**
+     * @brief Linear interpolation between a start vector to end vector with ratio
+     * @param _vect1 Start vector
+     * @param _vect2 End vector
+     * @param _ratio Ratio value (between 0 and 1)
+     * @return the vector result computed
+     */
 	static constexpr SelfType lerp(const SelfType& _vect1, const SelfType& _vect2, float _ratio) noexcept;
 
+	/**
+	 * @brief Spherical linear interpolation between a start vector to end vector with ratio
+	 * @param _vect1 Start vector
+	 * @param _vect2 End vector
+	 * @param _ratio Ratio value (between 0 and 1)
+	 * @return the vector result computed
+	 */
 	static constexpr SelfType slerp(const SelfType& _vect1, const SelfType& _vect2, float _ratio) noexcept;
 
+	/**
+	 * @brief Cross product the first vector param with the second
+	 * @param _vect1 First vector
+	 * @param _vect2 Second vector
+	 * @return the vector result computed
+	 */
 	static constexpr SelfType cross(const SelfType& _vect1, const SelfType& _vect2) noexcept;
 
+	/**
+	 * @brief Dot product 
+	 * @param _vect1
+	 * @param _vect2
+	 * @return
+	 */
 	static constexpr Type dot(const SelfType& _vect1, const SelfType& _vect2) noexcept;
 #pragma endregion
 
